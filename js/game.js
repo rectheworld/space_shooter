55c1bdad19cab17cb2634e91a349daf9cede60b1
Kreston  Shirley <notserk@Krestons-MBP.fios-router.home>
2016-12-07 02:42:04 -0500

    Added basic shooting mechanics

--- js/game.js
+++ js/game.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(640, 380, Phaser.AUTO, 'game-canvas', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(640, 380, Phaser.AUTO, 'game-canvas', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
     //The sprites need to be loaded in order so in our case we need to 
@@ -15,13 +15,22 @@ function preload() {
 
 var scrolling;
 var main_player;
-var controls
+var controls;
+
+
+var bullets;
+var fireButton;
+var nextFire = 0; 
+var bulletTime = 0;
 
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    
+    //Add Tile background to give scrolling effect
     scrolling = game.add.tileSprite(0, 0, 800, 600, 'background');
     
     //Create player object
-    main_player = game.add.sprite(0, 380/2, 'main_player');
+    main_player = game.add.sprite(50, 380/2, 'main_player');
     //Double Check to see what this means 
     //main_player.anchor.setTo(0.5, 0.5);
     game.physics.enable(main_player, Phaser.Physics.ARCADE);
@@ -29,10 +38,23 @@ function create() {
     
     //Add the enemies 
     
-    //Add the 
+    //Add Bullets 
+    bullets = game.add.group();
+    bullets.enableBody = true; 
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    
+    bullets.createMultiple(20, 'bullet');
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 1);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
+    
+    
+    
     
     //Add the controls
     controls = game.input.keyboard.createCursorKeys();
+    fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 function update() {
@@ -48,16 +70,38 @@ function update() {
     main_player.body.velocity.setTo(0, 0);
     
     //If left is pressed
-    if(controls.left.isDown){
+    if(controls.up.isDown){
         main_player.body.velocity.y = -200;
     }
     
-    else if(controls.right.isDown){
+    else if(controls.down.isDown){
         main_player.body.velocity.y = 200;
-    }     
+    }
+    
+    //Needs to be in it's own if statement 
+    if(fireButton.isDown){
+        //game.debug.text('Fire Pressed ' + fireButton.isDown, 32, 32);
+        fire();
+        
+    }
 }
 
-
+function fire(){
+    if(game.time.now > bulletTime){
+        
+        //Grabs bullets from pool
+        bullet = bullets.getFirstExists(false);
+        
+        if(bullet){
+            bullet.reset(main_player.x + 10, main_player.y + 5);
+            bullet.body.velocity.x = 400;
+            bulletTime = game.time.now + 200;
+        }
+    }
+}
+function render(){
+    //game.debug.text('Game Time ' + game.time.now, 100, 100);
+}
 
 
 /*
